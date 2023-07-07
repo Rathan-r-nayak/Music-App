e4afe57a092eb9862fd8647f3245485cf10b90e4
Rathan-r-nayak <rathannayak300@gmail.com>
2023-07-07 10:53:32 +0530

    complete music player app

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -1,181 +1,88 @@
 package com.example.musicapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
-
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
+import android.widget.Toast;
 
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import com.karumi.dexter.Dexter;
-import android.os.Environment;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<File> mySongs;
-    //    ListView listView;
-//    String[] items;
-    private ListView listView;
-    private ArrayList<String> songList;
-    private ArrayAdapter<String> adapter;
 
-    private static final int READ_EXTERNAL_STORAGE_PERMISSION_REQUEST = 1;
-
+    RecyclerView recyclerView;
+    TextView noMusicTextView;
+    ArrayList<AudioModel> songsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listViewSong);
+        recyclerView = findViewById(R.id.recycler_view);
+        noMusicTextView = findViewById(R.id.no_songs_text);
 
-        // Check if the READ_EXTERNAL_STORAGE permission is granted
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted, request it
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    READ_EXTERNAL_STORAGE_PERMISSION_REQUEST);
-        } else {
-            // Permission is already granted, fetch the songs
-            fetchSongs();
+        if(checkPermission() == false){
+            requestPermission();
+            return;
         }
+
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION
+        };
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC +" != 0";
+
+        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,projection,selection,null,null);
+        while(cursor.moveToNext()){
+            AudioModel songData = new AudioModel(cursor.getString(1),cursor.getString(0),cursor.getString(2));
+            if(new File(songData.getPath()).exists())
+                songsList.add(songData);
+        }
+
+        if(songsList.size()==0){
+            noMusicTextView.setVisibility(View.VISIBLE);
+        }else{
+            //recyclerview
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new MusicListAdapter(songsList,getApplicationContext()));
+        }
+
+    }
+
+    boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(result == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Toast.makeText(MainActivity.this,"READ PERMISSION IS REQUIRED,PLEASE ALLOW FROM SETTTINGS",Toast.LENGTH_SHORT).show();
+        }else
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == READ_EXTERNAL_STORAGE_PERMISSION_REQUEST) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, fetch the songs
-                fetchSongs();
-            } else {
-                // Permission denied, handle accordingly (e.g., show an error message)
-            }
+    protected void onResume() {
+        super.onResume();
+        if(recyclerView!=null){
+            recyclerView.setAdapter(new MusicListAdapter(songsList,getApplicationContext()));
         }
     }
-
-    private void fetchSongs() {
-        // Create an ArrayList to store the list of songs
-        songList = new ArrayList<>();
-
-        // Retrieve the songs using the MediaStore
-        Cursor cursor = getContentResolver().query(
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                null,
-                null,
-                null,
-                null
-        );
-
-        if (cursor != null && cursor.moveToFirst()) {
-            int titleColumn = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
-
-            int columnIndex;
-
-            do {
-                // Get the title of the song and add it to the list
-                String title = cursor.getString(titleColumn);
-                songList.add(title);
-
-            } while (cursor.moveToNext());
-            System.out.println(songList);
-
-            cursor.close();
-
-
-
-
-//            System.out.println("------------------------------------------------------------");
-////            mySongs= fetSongs(Environment.getExternalStorageDirectory());
-////            String[] items= new String[mySongs.size()];
-//            System.out.println("------------------------------------------------------------"+mySongs.size());
-        }
-
-        // Create the ArrayAdapter and set it on the ListView
-//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, songList);
-//        listView.setAdapter(adapter);
-
-        customAdapter customAdapter=new customAdapter();
-        listView.setAdapter(customAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String songName=(String) listView.getItemAtPosition(position);
-//                startActivity(new Intent(getApplicationContext(),PlayerActivity.class).putExtra("songs",songList).putExtra("songname",songName).putExtra("pos",position));
-
-                Intent intent= new Intent(MainActivity.this, PlayerActivity.class);
-                String currentSong= listView.getItemAtPosition(position).toString();
-                intent.putExtra("songs", songList);
-                intent.putExtra("songname", currentSong);
-                intent.putExtra("pos", position);
-                startActivity(intent);
-
-            }
-        });
-
-
-
-
-    }
-
-    class customAdapter extends BaseAdapter
-    {
-
-        @Override
-        public int getCount() {
-            return songList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View myView=getLayoutInflater().inflate(R.layout.list_item,null);
-            TextView textsong=myView.findViewById(R.id.txtsongname);
-            textsong.setSelected(true);
-            textsong.setText(songList.get(position));
-            return myView;
-        }
-    }
-
-
-
-
-
-
-
-
 }
